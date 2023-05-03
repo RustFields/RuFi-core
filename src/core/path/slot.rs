2b80c4142e0cc6a869d4a772c74a10ffe9b6fad7
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-03 11:29:58 +0200

    feat: added Exchange primitive in Slot

--- src/core/path/slot.rs
+++ src/core/path/slot.rs
@@ -6,15 +6,17 @@ pub mod slot {
         Nbr(i32),
         Rep(i32),
         Branch(i32),
+        Exchange(i32),
     }
 }
 
 impl Slot{
-    fn to_str(&self) -> String {
+    pub fn to_str(&self) -> String {
         match self {
             Slot::Nbr(index) => "Nbr(".to_owned()+&index.to_string()+")",
             Slot::Rep(index) => "Rep(".to_owned()+&index.to_string()+")",
             Slot::Branch(index) => "Branch(".to_owned()+&index.to_string()+")",
+            Slot::Exchange(index) => "Exchange(".to_owned()+&index.to_string()+")",
         }
     }
 }
@@ -28,9 +30,11 @@ mod test{
         let nbr = Slot::Nbr(0);
         let rep = Slot::Rep(0);
         let branch = Slot::Branch(0);
+        let exchange = Slot::Exchange(0);
         assert_eq!(nbr, Slot::Nbr(0));
         assert_eq!(rep, Slot::Rep(0));
         assert_eq!(branch, Slot::Branch(0));
+        assert_eq!(exchange, Slot::Exchange(0));
     }
 
     #[test]
@@ -38,8 +42,10 @@ mod test{
         let nbr = Slot::Nbr(0);
         let rep = Slot::Rep(0);
         let branch = Slot::Branch(0);
+        let exchange = Slot::Exchange(0);
         assert_eq!(nbr.to_str(), "Nbr(0)");
         assert_eq!(rep.to_str(), "Rep(0)");
         assert_eq!(branch.to_str(), "Branch(0)");
+        assert_eq!(exchange.to_str(), "Exchange(0)");
     }
 }
