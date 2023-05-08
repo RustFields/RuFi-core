e35bb7c7fcb1988c6a04a0162af4dc05b355d8d8
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-08 10:24:47 +0200

    ci: fix Semantic Release commit for updating version in Cargo.toml

--- release.config.js
+++ release.config.js
@@ -2,13 +2,13 @@ const config = require('semantic-release-preconfigured-conventional-commits')
 const verifyReleaseCommands = `
 sed -i 's/version.*/version = "\${nextRelease.version}"/g' Cargo.toml || exit 1
 git add -A || exit 2
-git commit -m "chore(release)!: [skip ci] \${nextRelease.version} released" || exit 3
+git commit -m "chore: [skip ci] update version in Cargo.toml" || exit 3
 cargo publish --dry-run || exit 4
 `
 const prepareCommands = `
 sed -i 's/version.*/version = "\${nextRelease.version}"/g' Cargo.toml || exit 1
 git add -A || exit 2
-git commit -m "chore(release)!: [skip ci] \${nextRelease.version} released" || exit 3
+git commit -m "chore: [skip ci] update version in Cargo.toml" || exit 3
 git push --force origin || exit 4
 `
 const publishCommands = `
