2f508536e652b7c7b8f82dd449f758cf30af5078
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-05-09 10:16:26 +0200

    feat: add method to create empty export and new export from path, update test

--- src/core/export.rs
+++ src/core/export.rs
@@ -1,25 +1,33 @@
+use std::any::Any;
 use std::collections::HashMap;
 use crate::core::export::export::Export;
 use crate::core::path::path::path::Path;
 
 pub mod export{
+    use std::any::Any;
     use std::collections::HashMap;
     use crate::core::path::path::path::Path;
 
-    #[derive(Debug, Clone, PartialEq)]
-    pub struct Export<A>{
-        pub map: HashMap<Path, A>,
+    #[derive(Debug)]
+    pub struct Export{
+        pub map: HashMap<Path, Box<dyn Any>>,
     }
 }
 
-impl<A: Clone + PartialEq> Export<A>{
-    pub fn new() -> Self{
+impl Export{
+    pub fn new_empty_export() -> Self{
         Export{
             map: HashMap::new(),
         }
     }
 
-    pub fn root(&self) -> A {
+    pub fn new<A>(path: Path, value: A) -> Self {
+        Export{
+            map: HashMap::from([(path.clone(), value.clone())]),
+        }
+    }
+
+    pub fn root<A>(&self) -> A {
         self.get(Path::new(Vec::new())).unwrap()
     }
 
@@ -58,46 +66,59 @@ mod tests {
         use super::*;
 
         #[test]
+        fn test_new_empty() {
+            let export = Export::new_empty_export();
+            assert_eq!(export.map.is_empty(), true)
+        }
+
+        #[test]
+        fn test_new() {
+            let export = Export::new(Path::new(vec![Nbr(0), Rep(0)]), 10);
+            assert_eq!(export.map.len(), 1);
+        }
+
+        #[test]
         fn test_put() {
-            let mut export = Export::new();
-            let path = Path::new(vec![Nbr(0), Rep(0)]);
-            export.put(path, 10);
-            assert_eq!(export.paths().len(), 1);
+            let export1 = Export::new(Path::new(vec![Nbr(0)]), 10);
+            let path = Path::new(vec![Rep(0), Nbr(0)]);
+            let export2 = export1.put(path, 20);
+            assert_eq!(export2.paths().len(), 2);
         }
 
         #[test]
         fn test_get() {
-            let mut export = Export::new();
             let path = Path::new(vec![Nbr(0), Rep(0)]);
-            export.put(path.clone(), 10);
+            let export = Export::new(path, 10);
             assert_eq!(export.get(path.clone()).unwrap(), 10);
         }
 
         #[test]
         fn test_root() {
-            let mut export:Export<Option<i32>> = Export::new();
-            let path = Path::new(Vec::new());
-            export.put(path.clone(), Some(10));
-            assert_eq!(export.root(), Some(10));
+            let export = Export::new(Path::new(Vec::new()), 10);
+            assert_eq!(export.root(), 10);
+        }
+
+        #[test]
+        fn test_paths(){
+            let path = Path::new(vec![Nbr(0), Rep(0)]);
+            let export = Export::new(path.clone(), 10);
+            let hm = HashMap::from([(path.clone(), 10)]);
+            assert_eq!(export.paths().eq(&hm), true);
         }
 
         #[test]
         fn test_eq() {
-            let mut export1 = Export::new();
-            let mut export2 = Export::new();
             let path = Path::new(vec![Nbr(0), Rep(0)]);
-            export1.put(path.clone(), 10);
-            export2.put(path.clone(), 10);
+            let export1 = Export::new(path.clone(), 10);
+            let export2 = Export::new(path.clone(), 10);
             assert_eq!(export1.eq(export2), true);
         }
 
         #[test]
         fn test_to_str() {
-            let mut export = Export::new();
             let path = Path::new(vec![Nbr(0), Rep(0)]);
-            export.put(path.clone(), 10);
-            assert_eq!(export.to_str(), "P://Nbr(0)/Rep(0)\n");
-        }
+            let export = Export::new(path.clone(), 10);
+            assert_eq!(export.to_str(), "P://Nbr(0)/Rep(0) -> 10, "); }
     }
 
 }
