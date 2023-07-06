1300524245165508434c8030749398b4472e9524
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-07-06 15:34:38 +0200

    refactor: round now returns also the result of the computation

--- src/core/lang/execution.rs
+++ src/core/lang/execution.rs
@@ -3,7 +3,8 @@ use crate::core::vm::round_vm::round_vm::RoundVM;
 pub fn round<A: Copy + 'static>(vm: RoundVM, program: impl Fn(RoundVM) -> (RoundVM, A)) -> (RoundVM, A) {
     let (mut vm_, res) = program(vm);
     vm_.register_root(res);
-    vm_
+    let res = vm_.export_data().root::<A>().clone();
+    (vm_, res)
 }
 
 #[cfg(test)]
