655698ffc1f813104ba84dc462850192d2f6f4c6
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-07-13 16:49:13 +0200

    feat: implemented functions to manipulate multiple aggregate programs

--- src/core/lang/execution.rs
+++ src/core/lang/execution.rs
@@ -7,6 +7,28 @@ pub fn round<A: Copy + 'static>(vm: RoundVM, program: impl Fn(RoundVM) -> (Round
     (vm_, res)
 }
 
+pub fn compose<A, F, G>(expr1: F, expr2: G) -> impl Fn(RoundVM) -> (RoundVM, A)
+where
+    F: Fn(RoundVM) -> (RoundVM, A),
+    G: Fn(RoundVM, A) -> (RoundVM, A),
+{
+    move |vm| {
+        let (vm_, res) = expr1(vm);
+        expr2(vm_, res)
+    }
+}
+
+pub fn combine<A, F, G, H>(vm: RoundVM, expr1: F, expr2: G, comb: H) -> (RoundVM, A)
+where
+    F: Fn(RoundVM) -> (RoundVM, A),
+    G: Fn(RoundVM) -> (RoundVM, A),
+    H: Fn(A, A) -> A,
+{
+    let (vm_, res1) = expr1(vm);
+    let (vm__, res2) = expr2(vm_);
+    (vm__, comb(res1, res2))
+}
+
 #[cfg(test)]
 mod test {
     use crate::core::context::context::Context;
