139603707d865b71c74c98fc2f81384734e7a15b
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-10-04 11:03:00 +0200

    feat(test): add utility to create vms

--- src/core/lang/test/utils.rs
+++ src/core/lang/test/utils.rs
@@ -4,13 +4,29 @@ use crate::core::lang::execution::round;
 use crate::core::vm::round_vm::round_vm::RoundVM;
 
 pub fn init_vm() -> RoundVM {
-    let context = Context::new(0, Default::default(), Default::default(), Default::default());
-    let mut vm = RoundVM::new(context);
+    let context = Context::new(1, Default::default(), Default::default(), Default::default());
+    init_with_ctx(context)
+}
+
+pub fn init_with_ctx(ctx: Context) -> RoundVM {
+    let mut vm = RoundVM::new(ctx);
     vm.export_stack.push(Export::new());
     vm
 }
 
-fn assert_equivalence<A,F>(program_1: F, program_2: F) -> bool
+pub fn push_to_ctx<A: Copy + 'static>(mut ctx: Context, path: Path, val: A) -> Context {
+    let mut export = Export::new();
+    export.put(path, || val);
+    ctx.put_export(ctx.self_id, export);
+    ctx
+}
+
+pub fn vm(self_id: i32, local_sensor: HashMap<SensorId, Box<dyn Any>>, nbr_sensor: HashMap<SensorId, HashMap<i32, Box<dyn Any>>>, exports: HashMap<i32, Export>) -> RoundVM {
+    let context = Context::new(self_id, local_sensor, nbr_sensor, exports);
+    init_with_ctx(context)
+}
+
+pub fn assert_equivalence<A,F, G>(exec_order: Vec<i32>, nbrs: HashMap<i32, Vec<i32>>, program_1: F, program_2: G) -> bool
     where
         F: Fn(RoundVM) -> (RoundVM, A),
         A: Eq + Copy + 'static,
